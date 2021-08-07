0d54a0ff3044ea4c717e0940bb8ecdb036666122
JazmyneB <bradleyj1@email.arizona.edu>
2021-08-06 22:47:51 -0700

    added home controller and homepage view

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 const express = require('express');
 const routes = require('./controllers');
 //const session = require('express-session');
-//const exphs = require('express-handlebars');
+const exphs = require('express-handlebars');
+const hbs = exphs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 const sequelize = require('./config/connection');
 //const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
