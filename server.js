86b42d9b4d1f392aaf29011b4d0a34511b68caea
JazmyneB <bradleyj1@email.arizona.edu>
2021-08-09 14:47:57 -0700

    working on styling and button links

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//const path = require('path');
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const session = require('express-session');
@@ -27,6 +27,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(session(sess));
 
